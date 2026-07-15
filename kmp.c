ed257e91c48650e8d34cb00e5ab753b98f69d6e7
Vincent <2501828182@qq.com>
2026-07-16 00:55:02 +0800

    kmp算法的实现

---
+++ kmp.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<string.h>
+char s[]= "ABCABABCABX";
+char p[]= "ABCABY";
+int next[100];
+int Next(char* p, int* next) {
+	int i = 1;
+	int j = 0;
+	next[0] = 0;
+	int len = strlen(p);
+	for (i; i < len; i++) {
+		while (p[i] != p[j] && j > 0) {
+			j = next[j - 1];
+		}
+		if (p[i] == p[j]) {
+			j++;
+		}
+		next[i] = j;
+	}
+	return 1;
+}
+int position(char* s, char* p) {
+	int i = 0;
+	int j = 0;
+	int len = strlen(s);
+	int lenp = strlen(p);
+	for (i; i < len; i++) {
+		if (j == lenp) {
+			return i-j;
+		}
+		else if (s[i] == p[j]) {
+			j++;
+		}
+		else if (s[i] != p[j]&&j>0) {
+			j = next[j - 1];
+		}
+		else if (s[i] != p[j] && j == 0) {
+			continue;
+		}
+	}
+	return 0;
+}
